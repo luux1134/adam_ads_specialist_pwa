5def2f577c3f981e5021e8763e534c8afcbb52dc
luux1134 <gueperoux.lucas@gmail.com>
2026-04-30 15:58:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "adam-ads-specialist-v48";
+const CACHE_NAME = "adam-ads-specialist-v49";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
